a889a02665f487d32488bd382810105cd1f0ec85
Morgan Chan <99379848+GLMortyc@users.noreply.github.com>
2026-01-22 14:22:58 -0700

    Create planets.java

---
+++ weight/planets.java
@@ -0,0 +1,16 @@
+public class planets {
+    private String planet;
+    private double gravity;
+
+    public planets(String p, double g) {
+        planet = p;
+        gravity = g;
+
+    }
+    public String getPlanet() {
+        return planet;
+    }
+    public double getGravity() {
+        return gravity;
+    }
+}
